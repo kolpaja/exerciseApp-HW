d8174942f0c3747806cbbba63eaf208054d749cb
Sokol Paja <kolpaja28@gmail.com>
2021-05-11 02:03:19 +0200

    todo@get/adi/workouts

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const mongoose = require("mongoose")
-const bodyParser = require('body-parser');
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 mongoose
   .connect("mongodb://localhost:27017/workout", {
@@ -16,22 +16,32 @@ const db = require("./models");
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/exercise", (req, res) => res.sendFile(path.join(__dirname, "./public/exercise.html")))
+app.get("/exercise", (req, res) =>
+  res.sendFile(path.join(__dirname, "./public/exercise.html"))
+);
 
-app.get("/stats", (req, res) => res.sendFile(path.join(__dirname, "./public/stats.html")))
+app.get("/stats", (req, res) =>
+  res.sendFile(path.join(__dirname, "./public/stats.html"))
+);
 
-app.get("/", (req, res) => res.sendFile(path.join(__dirname, "./public/index.html")))
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "./public/index.html"))
+);
 
 app.post("/api/workouts", (req, res) => {
   db.Workout.create({}).then((m) => res.json(m));
-})
+});
 
 app.put("/api/workouts/:id", (req, res) => {
-  console.log('put', req.params.id);
-  console.log('req.body: ', req.body);
-  db.Workout.findByIdAndUpdate(req.params.id, { $push: { exercises: req.body } }, { new: true, runValidators: true }).then(data => res.json({}));
-})
+  db.Workout.findByIdAndUpdate(
+    req.params.id,
+    { $push: { exercises: req.body } },
+    { new: true, runValidators: true }
+  ).then((data) => res.json({}));
+});
 
-
+app.get("/api/workouts/range", async (req, res) => {
+  db.Workout.find({ day: { $lte: new Date() } });
+});
 
 app.listen(3000, () => console.log("Connected on port: 3000"));
